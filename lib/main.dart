8953b1a245c688fc4f692103f150b044a41c8f38
rishi9953 <rk5680179@gmail.com>
2021-05-13 21:30:14 +0530

    update

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:personal_designing/page1.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Personal Designing",
+      home: Page1(),
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
