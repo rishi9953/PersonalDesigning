9743f4839d62946aef1446f650956f66c771ead7
rishi9953 <rk5680179@gmail.com>
2021-05-30 20:17:11 +0530

    update

--- lib/FirstPage.dart
+++ lib/FirstPage.dart
@@ -11,53 +11,61 @@ class _FirstPageState extends State<FirstPage> {
     return Scaffold(
       body: Container(
         alignment: Alignment.center,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Stack(
-              children: [
-                Container(
-                  child: CircleAvatar(
-                    backgroundImage: NetworkImage(
-                        'https://img.favpng.com/8/20/24/computer-icons-online-shopping-png-favpng-QuiWDXbsc69EE92m3bZ2i0ybS.jpg'),
-                    backgroundColor: Colors.red,
-                    radius: 70,
-                  ),
-                ),
-              ],
-            ),
-            SizedBox(
-              height: 100,
-            ),
-            Container(
-              width: 300,
-              child: ElevatedButton(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            children: [
+              SizedBox(
+                height: 50,
+              ),
+              Text(
+                'Welcome ',
+                style: Theme.of(context).textTheme.headline6,
+              ),
+              SizedBox(height: 10),
+              Text(
+                'Automatic identify verification which enables you to verify your identify',
+                textAlign: TextAlign.center,
+                style: Theme.of(context).textTheme.subtitle1,
+              ),
+              Image.asset('Assets/Images/5865.jpg'),
+              // Stack(
+              //   children: [
+              //     Container(
+              //       child: CircleAvatar(
+              //         backgroundImage: NetworkImage(
+              //             'https://img.favpng.com/8/20/24/computer-icons-online-shopping-png-favpng-QuiWDXbsc69EE92m3bZ2i0ybS.jpg'),
+              //         backgroundColor: Colors.red,
+              //         radius: 70,
+              //       ),
+              //     ),
+              //   ],
+              // ),
+              SizedBox(
+                height: 10,
+              ),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                    side: BorderSide(width: 1.0, color: Colors.black)),
                 onPressed: () {},
-                child: Text('Login'),
-                style: ButtonStyle(
-                  shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                    RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                  ),
+                child: Container(
+                  alignment: Alignment.center,
+                  width: 250,
+                  child: Text('Login'),
                 ),
               ),
-            ),
-            Container(
-              width: 300,
-              child: ElevatedButton(
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                    side: BorderSide(width: 1.0, color: Colors.black)),
                 onPressed: () {},
-                child: Text('Sign Up'),
-                style: ButtonStyle(
-                  shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                    RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                  ),
+                child: Container(
+                  alignment: Alignment.center,
+                  width: 250,
+                  child: Text('Sign Up'),
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
